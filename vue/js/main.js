a5f019b9e5cd2298538de9ceadc11ec4b86acf22
MyaOks <oksana.myasnikova.2003@mail.ru>
2024-05-02 13:00:24 +0700

    Закончена форма создания карточки с заметкой и начало работы с колонками и карточками

--- vue/js/main.js
+++ vue/js/main.js
@@ -16,7 +16,7 @@ Vue.component('menu_tabs', {
 
     data() {
         return {
-            tabs: ['Добавить заметку', 'Все заметки', 'Корзина'],
+            tabs: ['Добавить заметку', 'Все заметки'],
             selectedTab: 'Добавить заметку',
         }
     },
@@ -30,26 +30,40 @@ Vue.component('menu_tabs', {
 
 Vue.component('add_card', {
     template: `
-        <div v-show="tab == 'Добавить заметку'">
-            <form>
-                <h1>Создать заметку</h1>
+        <form class="form_add_cart" v-show="tab == 'Добавить заметку'">
+            <h1>Создать заметку</h1>
+            <p v-if="errors.length">
+                <b>Пожалуйста исправте следующие ошибки:</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
+            <div>
+                <label for="title_card">Заголовок</label>
+                <input id="title_card" type="text" maxlength="50" placeholder="Новая заметка" v-model="card.title">
+            </div>
+            <div>
+                <label for="body_card">Что там по делам?</label>
+                <textarea rows="5" v-model="card.text" placeholder="Можете что-нибудь здесь написать"></textarea>
+            </div>
+            <div>
                 <div>
-                    <lable for="title_card">Заголовок</lable>
-                    <input id="title_card" type="text" maxlength="50" v-model="card.title">
-                </div>
-                <div>
-                    <lable for="body_card">Что там по делам?</lable>
-                    <input type="text" style="border:none; outline:none">
-                </div>
-                <div v-for="(tasks, index) in card.tasks">
-                    <div>
+                    <h4 v-show="card.tasks.length < 3">Задач должно быть как минимум три</h4>
+                    <h4 v-show="card.tasks.length == 5">Достигнуто максимальное количество задач</h4>
+                    <div v-for="(task, index) in card.tasks">
                         <input type="checkbox" disabled>
-                        <input type="text" placeholder="Задача №1">
+                        <input type="text" placeholder="Задача" v-model="task.title">
                     </div>
                 </div>
-            </form>
-        </div>
+                <div>
+                    <button type="button" @click="addTask" :class="{disabledButtonDelAddTask: card.tasks.length == 5}">Добавить задачу</button>
+                    <button type="button" @click="delTask" :class="{disabledButtonDelAddTask: card.tasks.length == 0}">Удалить задачу</button>
+                </div>
+            </div>
+            <button type="button" @click="addCard" :class="{disabledButtonAddCard: card.tasks.length < 3}">Добавить заметку</button>
+        </form>
     `,
+
     props: {
         tab: {
             required: true
@@ -60,27 +74,77 @@ Vue.component('add_card', {
         return {
             errors: [],
             card: {
-                title: "Новая заметка",
-                tasks: []
+                title: "",
+                text: "",
+                tasks: [
+                    {title: "", done: false},
+                    {title: "", done: false},
+                    {title: "", done: false},
+                ],
+                date: new Date().toLocaleTimeString() + ' ' + new Date().toLocaleDateString()
+            },
+        }
+     },
+
+    methods: {
+        addTask() {
+            if(this.card.tasks.length < 5) {
+                this.card.tasks.push({title:"", done: false})
+            }
+        },
+
+        delTask() {
+            this.card.tasks.pop()
+        }, 
+
+        addCard() {
+            if(this.card.title && this.card.tasks.filter(tasks => tasks.title).length == this.card.tasks.length){
+                let bodyCard = {
+                    title: this.card.title,
+                    text: this.card.text, 
+                    task: this.card.tasks,
+                    date: this.card.date
+                }
+                this.$emit('add-task', bodyCard);
+                location.reload();
+            } else {
+                if(!this.card.title) this.errors.push("Заголовок не заполнен")
+                if(this.card.tasks.filter(tasks => tasks.title).length != this.card.tasks.length) this.errors.push("Какой-то из пунктов списка задач пуст")
             }
         }
-     }
-
-         //  methods: {
-    //     addTask() {
-    //         if (this.card.tasks.length < 5){
-    //             this.card.tasks.push({title:})
-    //         }
-    //     }
-    //  },
+     },
 })
 
 Vue.component('column', {
 
+    props: {
+        column: {
+            title: '',
+            cards: [],
+            date: ''
+        },
+        tab: {
+            required: true
+        }
+    },
+
+    template: `
+        <div class="column" v-show="tab == 'Все заметки'">
+            <h2>{{ column.title }}</h2>
+            <div>
+                <card v-for="(card, index) in column.cards" :key="index"></card>
+            </div>
+        </div>
+    `,
+
+    updated() {
+        this.$emit('save')
+    },
+
 }),
 
-Vue.component('task', {
-
+Vue.component('card', {
+    
 })
 
 let app = new Vue({
@@ -110,6 +174,10 @@ let app = new Vue({
     methods: {
         handleTabChange(tab) {
             this.tab = tab;
-        }
+        },
+
+        save() {
+            localStorage.setItem('columns', JSON.stringify(this.columns))
+        },
     }
 })
